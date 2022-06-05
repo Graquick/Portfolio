83f3b59e6f0b90788fe8aba2f7fc134984bca298
Graquick <84386995+Graquick@users.noreply.github.com>
2022-06-05 19:30:04 +0300

    major updates

---
+++ .history/pages/_document_20220605170519.js
@@ -0,0 +1,13 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head />
+      <body style={{ fontFamily: 'Raleway', 'sans-serif' }}>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
