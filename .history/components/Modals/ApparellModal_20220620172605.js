08f4cecb2fb9676b1006520ba3433e42e0cd90ba
Graquick <84386995+Graquick@users.noreply.github.com>
2022-06-20 18:14:10 +0300

    home page dark mode done

---
+++ .history/components/Modals/ApparellModal_20220620172605.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Link from 'next/link';
+import { motion, AnimatePresence } from 'framer-motion';
+import { Icon } from '@iconify/react';
+import styles from '../../styles/Modals.module.css';
+
+const ApparellModal = ( {showCModal, setShowCModal} ) => {
+
+    const backDrop = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 }
+    }
+
+    const modal = {
+        hidden: {
+            scale: 0,
+            opacity: 0
+        },
+        visible: {
+            scale: 1,
+            opacity: 1,
+            transition: { delay: .25 }
+        }
+    }
+
+    return (
+        <AnimatePresence exitBeforeEnter>
+            { showCModal && (
+                <motion.div className="backdrop"
+                    variants={backDrop}
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                >
+                    <motion.div
+                        variants={modal}
+                        className={styles.apparellModal}
+                    >
+                        <div>
+                            <h1 className="font-light font-Roboto text-16 ">.apparell</h1>
+                            <p>A beautiful E-commerce website with functional properties (e.g. lightbox product gallery).</p>
+                            <br />
+                            <p>Check out the full description at my projects page.</p>
+                            <motion.div className={styles.closeBar}
+                            >
+                                <Icon icon="clarity:window-min-line" style={{ color: "#a8a8a8" }}/>
+                                <Icon icon="mdi:window-restore" style={{ color: "#a8a8a8" }}/>
+                                <motion.div className={styles.closeBtn} onClick={() => {setShowCModal(false)}}
+                                    whileTap={{ scale: 0.5, transition: { duration: .1 } }}
+                                >
+                                    <Icon icon="eva:close-fill"/>
+                                </motion.div>
+                            </motion.div>
+                        </div>
+                    </motion.div>
+                </motion.div>
+            ) }
+        </AnimatePresence>
+     );
+}
+ 
+export default ApparellModal;
---
+++ .history/components/Modals/ApparellModal_20220620173733.js
@@ -0,0 +1,62 @@
+import * as React from 'react';
+import Link from 'next/link';
+import { motion, AnimatePresence } from 'framer-motion';
+import { Icon } from '@iconify/react';
+import styles from '../../styles/Modals.module.css';
+
+const ApparellModal = ( {showCModal, setShowCModal} ) => {
+
+    const backDrop = {
+        hidden: { opacity: 0 },
+        visible: { opacity: 1 }
+    }
+
+    const modal = {
+        hidden: {
+            scale: 0,
+            opacity: 0
+        },
+        visible: {
+            scale: 1,
+            opacity: 1,
+            transition: { delay: .25 }
+        }
+    }
+
+    return (
+        <AnimatePresence exitBeforeEnter>
+            { showCModal && (
+                <motion.div className="backdrop"
+                    variants={backDrop}
+                    initial="hidden"
+                    animate="visible"
+                    exit="hidden"
+                >
+                    <motion.div
+                        variants={modal}
+                        className={styles.apparellModal}
+                    >
+                        <div>
+                            <h1 className="relative mb-4 font-light text-black font-roboto text-[4rem]">.apparell</h1>
+                            <p className="text-[1.3rem] font-medium ">A beautiful E-commerce website with functional properties (e.g. lightbox product gallery).</p>
+                            <br />
+                            <p>Check out the full description at my projects page.</p>
+                            <motion.div className={styles.closeBar}
+                            >
+                                <Icon icon="clarity:window-min-line" style={{ color: "#a8a8a8" }}/>
+                                <Icon icon="mdi:window-restore" style={{ color: "#a8a8a8" }}/>
+                                <motion.div className={styles.closeBtn} onClick={() => {setShowCModal(false)}}
+                                    whileTap={{ scale: 0.5, transition: { duration: .1 } }}
+                                >
+                                    <Icon icon="eva:close-fill"/>
+                                </motion.div>
+                            </motion.div>
+                        </div>
+                    </motion.div>
+                </motion.div>
+            ) }
+        </AnimatePresence>
+     );
+}
+ 
+export default ApparellModal;
